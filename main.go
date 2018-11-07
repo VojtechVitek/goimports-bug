158c3acde68178a74be1e842c54bb800f8bef647
Vojtech Vitek <vojtech.vitek@visioncritical.com>
2018-11-07 17:31:56 -0500

    Explain the regression

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
+// This used to work until recently:
 import "github.com/VojtechVitek/goimports-bug/pkg/ibm"
+
+// Only explicit pkg rename works now:
+// import redhat "github.com/VojtechVitek/goimports-bug/pkg/ibm"
 
 func main() {
 	redhat.Foo()
